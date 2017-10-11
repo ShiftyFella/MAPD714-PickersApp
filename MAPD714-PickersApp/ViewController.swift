12eab4dc860d7bc442f13a5742a486d466161109
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-10-11 18:45:43 -0400

    Initial Commit

---
+++ MAPD714-PickersApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD714-PickersApp
+//
+//  Created by Viktor Bilyk on 2017-10-11.
+//  Copyright © 2017 Shifty Land LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
