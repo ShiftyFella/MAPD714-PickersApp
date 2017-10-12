7c824ebe8601a170996700ea582a6436a71d8340
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-10-11 21:12:17 -0400

    Single Picker logic added, Date picker logic changed

--- MAPD714-PickersApp/DatePickerViewController.swift
+++ MAPD714-PickersApp/DatePickerViewController.swift
@@ -7,21 +7,25 @@
 //
 
 import UIKit
+import Foundation
 
 class DatePickerViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let date=NSDate()
         datePicker.setDate(date as Date, animated: false)
+        let seconds: TimeInterval = -5*60*60
+        datePicker.timeZone = TimeZone(secondsFromGMT: Int(seconds))
         // Do any additional setup after loading the view.
     }
     @IBAction func onButtonPressed(_ sender: UIButton) {
+       // let difference: TimeInterval? =
         let date = datePicker.date
         let message = "The date and Time Selected is \(date)"
-        
         let alert = UIAlertController(title: "Date and Time Selected", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "That's so true!", style: .default, handler: nil)
         
--- MAPD714-PickersApp/SingleComponentPickerViewController.swift
+++ MAPD714-PickersApp/SingleComponentPickerViewController.swift
@@ -8,28 +8,42 @@
 
 import UIKit
 
-class SingleComponentPickerViewController: UIViewController {
+class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var SinglePicker: UIPickerView!
+    
+    private let characterNames = ["Han Solo", "Luke Skywalker", "Chuwi", "BB8", "General", "Lando"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        
+        let row = SinglePicker.selectedRow(inComponent: 0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)"
+        
+        let alert = UIAlertController(title: title, message: "Thank you for choosing", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "You're welcome!", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //adhere with protocols
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return characterNames[row]
+    }
+    
 }
